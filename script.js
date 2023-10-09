2d340c38950ab3b903a26323e9817301425c2b76
Amélie Guyot <amelieguyot7@gmail.com>
2023-10-09 18:44:16 +0200

    first step

---
+++ script.js
@@ -0,0 +1,4 @@
+const copy = document.querySelector('.logos-slide').cloneNode(true);
+document.querySelector(".logos").appendChild(copy);
+const copyThird = document.querySelector('.logos-slide').cloneNode(true);
+document.querySelector(".logos").appendChild(copyThird);
